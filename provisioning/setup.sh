ada88b370b9eccefbd329d3f0194bb80ef6aeb35
vinc3nt <vinc3nt@riseup.net>
2014-03-24 19:29:19 +0000

    provisioning output

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-
-echo "[Freepto] installing Freeptobuild tool..."
+echo "==> freepto: installing Freeptobuild tool..."
 install -o root -g root -m 744 /vagrant/provisioning/freeptobuild /usr/local/bin/freeptobuild
 install -o root -g root -m 644 /vagrant/provisioning/freeptobuild.cfg /etc/freeptobuild.cfg
-echo "Done."
-echo ""
-echo "[Freepto] installing custom configuration file..."
+
+echo "==> freepto: installing custom configuration file..."
 install -o root -g root -m 644 /vagrant/provisioning/gitconfig /etc/gitconfig
 install -o root -g root -m 644 /vagrant/provisioning/zshrc.local /etc/zsh/zshrc.local
 install -o root -g root -m 644 /vagrant/provisioning/zshrc /etc/zsh/zshrc
-echo "Done."
-echo ""
-echo "[Freepto] cloning AvANa git repository..."
+
+echo "==> freepto: cloning AvANa git repository..."
 git clone https://github.com/AvANa-BBS/freepto-lb /home/vagrant/freepto-lb
-echo "Done."
+
+echo "freepto-dev is ready"
