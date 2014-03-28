b0a396bef297705ecb41e163bb4b0957a79209ae
vinc3nt <vinc3nt@riseup.net>
2014-03-28 00:05:32 +0000

    tools for qemu/libvirt box

---
+++ packer/raw2box.sh
@@ -0,0 +1,31 @@
+# Doc references
+# http://docs.vagrantup.com/v2/boxes/format.html
+
+OUTPUT_DIR="output-freepto-qemu/"
+BUILDS_DIR="builds/libvirt"
+
+echo "Starting image conversion..."
+
+#qemu-img convert -c -O qcow2 ${OUTPUT_DIR}/freepto-dev.qcow2 ${BUILDS_DIR}/box.img
+
+echo "Creating box file..."
+
+cat > ${BUILDS_DIR}/Vagrantfile << END
+Vagrant.configure("2") do |config|
+        config.vm.provider :libvirt do |libvirt|
+          libvirt.disk_bus = 'ide'
+        end
+end
+END
+
+cat > ${BUILDS_DIR}/metadata.json << END
+{"provider":"libvirt","format":"qcow2","virtual_size":21}
+END
+
+cd ${BUILDS_DIR}
+tar czf freepto-libvirt.box metadata.json Vagrantfile box.img
+
+# clean
+rm -i metadata.json
+rm -i Vagrantfile
+rm -i box.img
