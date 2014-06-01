6d512c7c3fafd061727ecd19e08c56070be2a5b0
vinc3nt <vinc3nt@riseup.net>
2014-06-01 19:06:39 +0100

    added banner and ssh config

--- sync/provisioning/setup.sh
+++ sync/provisioning/setup.sh
@@ -7,9 +7,12 @@ echo "==> freepto: installing custom configuration file..."
 install -o root -g root -m 644 /vagrant/provisioning/gitconfig /etc/gitconfig
 install -o root -g root -m 644 /vagrant/provisioning/zshrc.local /etc/zsh/zshrc.local
 install -o root -g root -m 644 /vagrant/provisioning/zshrc /etc/zsh/zshrc
+install -o root -g root -m 644 /vagrant/provisioning/motd /etc/motd
+install -o root -g root -m 644 /vagrant/provisioning/sshd_config /etc/ssh/sshd_config
 install -o vagrant -g vagrant -m 644 /vagrant/provisioning/.zshrc /home/vagrant/.zshrc
 chsh -s /usr/bin/zsh vagrant
 chsh -s /usr/bin/zsh root
+/etc/init.d/ssh reload
 
 echo "==> freepto: cloning AvANa git repository..."
 git clone https://github.com/AvANa-BBS/freepto-lb /home/vagrant/freepto-lb
